2bb30828ea396430fd46353021d3002c3d98dc75
Félix Fischer <felix91gr@gmail.com>
2018-05-27 00:36:31 -0400

    Another one yet

--- shim.h
+++ shim.h
@@ -1,7 +1,10 @@
 #include <systemd/sd-journal.h>
+#include <stdio.h>
 
 extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
     // return sd_journal_print_with_location(priority, file, line, func, message);
+	printf("I'm sending %s to the system log!\n", message);
 	return sd_journal_print(priority, message);
+	printf("Log has been sent!\n");
 }
