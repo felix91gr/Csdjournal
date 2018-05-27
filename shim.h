73157617e90bd8e866164ffec5dc500699f6df5a
Félix Fischer <felix91gr@gmail.com>
2018-05-27 12:58:11 -0400

    Making a release for public query.

--- shim.h
+++ shim.h
@@ -3,8 +3,17 @@
 
 extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
-    // return sd_journal_print_with_location(priority, file, line, func, message);
-	printf("I'm sending %s to the system log!\n", message);
-	return sd_journal_print(priority, message);
+	printf("I'm sending %s to the system log, from %s, at %s : %s\n", message, file, func, line);
+    	
+    int i = 0;
+    int res;
+
+    for(i = 0; i < 30; i++) {
+    	res = sd_journal_print_with_location(i, file, line, func, message);
+    	printf("Got %i out of the system call!\n", res);
+    }
+
 	printf("Log has been sent!\n");
+
+	return res;
 }
