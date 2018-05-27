616d3c6459a326f63882844a9168399aaf054964
Félix Fischer <felix91gr@gmail.com>
2018-05-26 21:15:18 -0400

    ...

--- csd_journal.h
+++ csd_journal.h
@@ -1,6 +1,6 @@
 #include <systemd/sd-journal.h>
 
-extern inline
-int csd_journal_print_for_swift(int priority, const char *file, const char *line, const char *func, const char *format) {
-    return sd_journal_print_with_location(priority, file, line, func, format);
-}
+// extern inline
+// int csd_journal_print_for_swift(int priority, const char *file, const char *line, const char *func, const char *format) {
+//     return sd_journal_print_with_location(priority, file, line, func, format);
+// }
