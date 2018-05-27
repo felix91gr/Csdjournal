9dc07c0b64ba5a7ffb0833f68180f9f1b007e4d3
Félix Fischer <felix91gr@gmail.com>
2018-05-27 18:45:59 -0400

    IT WORKS

--- shim.h
+++ shim.h
@@ -6,17 +6,14 @@ extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
 	char fileStr[60];
 	char lineStr[60];
+	
 	strcpy (fileStr,"CODE_FILE=");
 	strncat (fileStr, file, 20);
 
 	strcpy (lineStr,"CODE_LINE=");
 	strncat (lineStr, line, 20);
 
-	printf("I'm sending %s to the system log, from %s, at %s : %s\n", message, file, func, line);
-    	
     int res = sd_journal_print_with_location(priority, fileStr, lineStr, func, message);
-
-	printf("Log has been sent!\n");
 
 	return res;
 }
