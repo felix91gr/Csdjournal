de8312b6ee4467e13efc0f217851cf9120e36816
Félix Fischer <felix91gr@gmail.com>
2018-05-26 23:36:51 -0400

    Trying other solutions.

--- shim.h
+++ shim.h
@@ -1,5 +1,6 @@
-#import <systemd/sd-journal.h>
+#include <systemd/sd-journal.h>
 
+extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
     return sd_journal_print_with_location(priority, file, line, func, message);
 	// return sd_journal_print(priority, message);
