1f9764e5342408765ce63b80c336c726185ee803
Félix Fischer <felix91gr@gmail.com>
2018-05-26 23:50:29 -0400

    Another one

--- shim.h
+++ shim.h
@@ -2,6 +2,6 @@
 
 extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
-    return sd_journal_print_with_location(priority, file, line, func, message);
-	// return sd_journal_print(priority, message);
+    // return sd_journal_print_with_location(priority, file, line, func, message);
+	return sd_journal_print(priority, message);
 }
