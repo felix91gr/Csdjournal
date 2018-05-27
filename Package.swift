14d7dc04b3280716725df66c9c1e35d4144e41a3
Félix Fischer <felix91gr@gmail.com>
2018-05-26 22:53:41 -0400

    Second restart. Basing this off of IBM-Swift's CCurl library

--- Package.swift
+++ Package.swift
@@ -5,5 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Csdjournal",
-    pkgConfig: "libsystemd"
-    )
+    
+    providers: [
+    	.apt(["libsystemd-dev"])
+    ],
+    
+    products: [
+    	.library(
+    		name: "CSDJournal",
+    		targets: ["CSDJournal"]
+    	)
+    ],
+    
+    targets: [
+    	.target(name: "CSDJournal")
+    ]
+)
