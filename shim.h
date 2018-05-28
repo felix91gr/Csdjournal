36e07704d0db9f17f35198f3e3fa629fd07d8129
Félix Fischer <felix91gr@gmail.com>
2018-05-28 14:55:36 -0400

    Testing separating functionality to avoid duplicating definitions

--- shim.h
+++ shim.h
@@ -7,29 +7,6 @@
 #include <string.h>
 
 extern inline
-int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
-	int lenFileStr = strlen(file);
-	int lenLineStr = strlen(line);
-
-	char prefixFile[] = "CODE_FILE=";
-	char prefixLine[] = "CODE_LINE=";
-
-	// Total length: size of both, plus 1 more for the null terminator character.
-	char *fileStr = malloc(sizeof(char) * (lenFileStr + strlen(prefixFile) + 1));
-	char *lineStr = malloc(sizeof(char) * (lenLineStr + strlen(prefixLine) + 1));
-
-	strcpy (fileStr, prefixFile);
-	strcat (fileStr, file);
-
-	strcpy (lineStr, prefixLine);
-	strcat (lineStr, line);
-
-	int res = sd_journal_print_with_location(priority, fileStr, lineStr, func, message);
-
-	free (fileStr);
-	free (lineStr);
-
-	return res;
-}
+int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message);
 
 #endif /* SDJournalHelpers_h */
