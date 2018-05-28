9cb934b20e3dd68c811dd31790f4bebf62099f2b
Félix Fischer <felix91gr@gmail.com>
2018-05-28 00:13:39 -0400

    Testing dynamic allocation of buffers.

--- shim.h
+++ shim.h
@@ -1,19 +1,30 @@
 #include <systemd/sd-journal.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 extern inline
 int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
-	char fileStr[60];
-	char lineStr[60];
-	
-	strcpy (fileStr,"CODE_FILE=");
-	strncat (fileStr, file, 20);
+	int lenFileStr = strlen(file);
+	int lenLineStr = strlen(line);
 
-	strcpy (lineStr,"CODE_LINE=");
-	strncat (lineStr, line, 20);
+	char prefixFile[] = "CODE_FILE=";
+	char prefixLine[] = "CODE_LINE=";
+
+	// Total length: size of both, plus 1 more for the null terminator character.
+	char *fileStr = malloc(sizeof(char) * (lenFileStr + strlen(prefixFile) + 1));
+	char *lineStr = malloc(sizeof(char) * (lenLineStr + strlen(prefixLine) + 1));
+
+	strcpy (fileStr, prefixFile);
+	strcat (fileStr, file);
+
+	strcpy (lineStr, prefixLine);
+	strcat (lineStr, line);
 
     int res = sd_journal_print_with_location(priority, fileStr, lineStr, func, message);
+
+    free (fileStr);
+    free (lineStr);
 
 	return res;
 }
