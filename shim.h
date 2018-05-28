2bc89bbb56bd010efc2916e07eb9dddc6fbfb362
Félix Fischer <felix91gr@gmail.com>
2018-05-28 16:26:59 -0400

    Testing with static inline

--- shim.h
+++ shim.h
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 
-extern inline int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
+static inline int csd_journal_print(int priority, const char *file, const char *line, const char *func, const char *message) {
 	int lenFileStr = strlen(file);
 	int lenLineStr = strlen(line);
 
